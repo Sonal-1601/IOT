6dfda45993bd9bf409518958c44b37f2a3e7861c
Sonal-1601 <71246954+Sonal-1601@users.noreply.github.com>
2020-09-14 11:31:32 +0530

    Create PRAC1.py

---
+++ PRAC1.py
@@ -0,0 +1,16 @@
+import telepot
+
+token ='1208283437:AAEysyY40C_RJ-49FSG-pgCbz9p0UxTMHjc'
+bot = telepot.Bot(token)
+print (bot.getMe())
+
+def handle(msg):
+  content_type = telepot.glance(msg)
+  chat_type=telepot.glance(msg)
+  chat_id=telepot.glance(msg)
+  print(content_type, chat_type, chat_id)
+
+  if content_type == 'text':
+    bot.sendMessage(chat_id, "You said '{}'".format(msg["text"]))
+    
+  bot.message_loop(handle)
